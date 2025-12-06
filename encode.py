dce505c4464239d5dcff7a8bdd995e288593da6d
gnd <peter@ftmo.com>
2025-12-06 12:11:36 +0100

    feat: finished 2.5

--- encode.py
+++ encode.py
@@ -1,9 +1,11 @@
 import os
+import tiktoken
 from tokenizer import TokenizerV1
 
 # some variables
 source_dir = "sources"
-tokenizer = TokenizerV1()
+#tokenizer = TokenizerV1()
+tokenizer = tiktoken.get_encoding("o200k_base")
 
 def import_sources(source_dir):
     for filename in os.listdir(source_dir):
@@ -12,22 +14,24 @@ def import_sources(source_dir):
         if os.path.isfile(filepath):
             with open(filepath, "r", encoding="utf-8") as f:
                 print(f"Opening {filepath} ..")
-                raw_text = f.read()
+                raw_text = f.read() + " <|endoftext|>"
                 tokenizer.encode(raw_text)
 
     # Save the generated vocabulary
-    tokenizer.save_vocab("objects/vocab.json")
+    # Only TokenizerV1
+    #tokenizer.save_vocab("objects/vocab.json")
 
     # test
-    print(f"All words: {len(tokenizer.str_to_int)}")
+    # Only TokenizerV1
+    #print(f"All words: {len(tokenizer.str_to_int)}")
 
 
 # import source texts from sources dir into proprocessed_texts array
 #import_sources(source_dir)
 
 # test
-tokenizer.load_vocab("objects/vocab.json")
-ids = tokenizer.encode("Hello, do you like tea?")
+# tokenizer.load_vocab("objects/vocab.json")
+ids = tokenizer.encode("Hello, do you like tea? <|endoftext|>", allowed_special={"<|endoftext|>"})
 print(ids)
 
 # test
